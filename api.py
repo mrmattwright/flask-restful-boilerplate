bdbb370728e8543ea2b15d92d4a804900aa7383a
Matt Wright <matt@sudden.co.nz>
2017-06-02 15:02:36 +0100

    messages controller fixed

--- api.py
+++ api.py
@@ -1,17 +1,13 @@
 from flask import Flask
 from flask_restful import Api
-from controllers import helloController
+from controllers import helloController, messageController
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 api = Api(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///static/db/test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
-
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 
 api.add_resource(helloController.HelloController, '/api/hello')
 api.add_resource(messageController.MessageController, '/messages')
