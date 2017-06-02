97e3a18c0170d9ec2b2772f9224625607cac2bab
Matt Wright <matt@sudden.co.nz>
2017-06-02 22:25:02 +0100

    First working version

--- api.py
+++ api.py
@@ -1,16 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 from controllers import helloController, messageController
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 api = Api(app)
-
-#db = SQLAlchemy(app)
 
 api.add_resource(helloController.HelloController, '/api/hello')
 api.add_resource(messageController.MessageController, '/messages')
+api.add_resource(messageController.MessagesController, '/messages/<message_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- controllers/messageController.py
+++ controllers/messageController.py
@@ -1,9 +1,34 @@
-from flask_restful import Resource
+from flask_restful import Resource, Api, reqparse
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import os
 
 class MessageController(Resource):
 
-    def get(self):
-        return {"response" : "messages get"}
-
     def post(self):
-        return {"response" : "messages post"}
+        parser = reqparse.RequestParser()
+        parser.add_argument('message', type=str, required=True, location='json')
+        args = parser.parse_args(strict=True)
+
+        url = os.environ.get('MONGO_URL')
+        client = MongoClient(url,connectTimeoutMS=30000,
+                             socketTimeoutMS=None,
+                             socketKeepAlive=True)
+        db = client.get_default_database()
+        # Test stuff
+        result = db.messagestore.insert({"message" : args.message})
+        print(result)
+        return {"digest" : str(result)}
+
+class MessagesController(Resource):
+
+    def get(self, message_id):
+        url = os.environ.get('MONGO_URL')
+        client = MongoClient(url,connectTimeoutMS=30000,
+                             socketTimeoutMS=None,
+                             socketKeepAlive=True)
+        db = client.get_default_database()
+        # Test stuff
+        result = db.messagestore.find_one({'_id': ObjectId(message_id)}, {'_id': False})
+        print(result)
+        return result
